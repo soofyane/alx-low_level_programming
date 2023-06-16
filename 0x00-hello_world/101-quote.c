5070bfd913bd10e2172e278176379336ce0510f5
soofyane <soufianehalloumi33@gmail.com>
2023-06-16 11:31:18 -0700

    Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error Without using puts and printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,7 @@
-#include <unistd.h> 
-#include <sys/syscall.h> 
-int main(void) 
-{ 
-    const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
-    syscall(SYS_write, 1, msg, sizeof(msg)); 
-    return 0; 
-} 
+#include <unistd.h>
+int main(void)
+{
+	char phrase="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	write(1, phrase, sizeof(phrase));
+	return (1)
+}
