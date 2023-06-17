b6f4d0797b066feba46a68feca0e074f26e53d1d
soofyane <soufianehalloumi33@gmail.com>
2023-06-16 18:43:06 -0700

    Determines either greater than 5, is less than 6, or is 0

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Determines either greater than 5, is less than 6, or is 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int ldn = n;
+
+	while (ldn > 10)
+	{
+		ldn = ldn /  10;
+	}
+	if (ldn > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, ldn);
+	}
+	else if (ldn == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, ldn);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ldn);
+	}
+	return (0);
+}
