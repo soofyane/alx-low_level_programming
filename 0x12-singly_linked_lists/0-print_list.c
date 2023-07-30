0a85ab9cee554e3d6cb221f6cc15e4926f089fab
soofyane <soufianehalloumi33@gmail.com>
2023-07-30 11:09:14 -0700

    Write a function that prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,22 @@
 #include "lists.h"
-/**
- * print_list - prints all the elements of a list_t list.
- * @h: singly linked list.
- * Return: number of elements in the list.
- */
 
+/**
+ *
+ *
+ *
+ */
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
-
-	nelem = 0;
-	while (h != NULL)
+	size_t count = 0;
+	if (h == NULL)
+		printf("empty list");
+	struct node *ptr = NULL;
+	ptr = h;
+	while (ptr != NULL)
 	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		nelem++;
+		count++;
+		printf("[%d] %s\n",ptr->len, ptr->str);
+		ptr = ptr->link;
 	}
-	return (nelem);
+	return (count);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,8 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+
+#ifndef LISTS.H
+#define LISTS.H
 
 /**
  * struct list_s - singly linked list
@@ -15,17 +14,11 @@
  */
 typedef struct list_s
 {
-char *str;
-unsigned int len;
-struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
+
 size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
 
-
-
-#endif
