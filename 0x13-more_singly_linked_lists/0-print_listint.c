14d7aa4acabdb86cd539e2a0af928555c6ab4bde
soofyane <soufianehalloumi33@gmail.com>
2023-08-01 11:12:19 -0700

    Write a function that prints all the elements of a listint_t list.

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * print_listint - function that prints all the elements of a listint_t list.
+ * print_listint - function that prints all the elements of a list
  * @h: head of the list
  * Return: count
  *
